d33c7fd969fd9f421c3ebf956a42ab1d4db2b590
= <=>
2014-10-07 21:34:15 -0300

    Borro al JSONObject

--- desapp-groupc-backend/src/main/java/ar/edu/unq/desapp/grupoc/web/rest/CategoryRest.java
+++ desapp-groupc-backend/src/main/java/ar/edu/unq/desapp/grupoc/web/rest/CategoryRest.java
@@ -1,7 +1,6 @@
 package ar.edu.unq.desapp.grupoc.web.rest;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -10,30 +9,18 @@ import javax.ws.rs.Produces;
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 import ar.edu.unq.desapp.grupoc.model.Category;
 import ar.edu.unq.desapp.grupoc.model.Egress;
 import ar.edu.unq.desapp.grupoc.model.Ingress;
 import ar.edu.unq.desapp.grupoc.services.CategoryService;
-import ar.edu.unq.desapp.grupoc.utils.JSONObject;
 
 @Service
 @Path("/categories")
 public class CategoryRest {
 
 	private CategoryService categoryService;
-	
-	@GET
-	@Path("/")
-	@Produces("application/json")
-	public String getCategoryById() throws JsonGenerationException, JsonMappingException, IOException{
-		List<Category> categorias = new ArrayList<Category>();
-		categorias.add(new Category("hola", null));
-		categorias.add(new Category("chau",null));
-		return JSONObject.getInstance().ObjectToJSON(categorias);
-	}
 	
 	@GET
 	@Path("/all")

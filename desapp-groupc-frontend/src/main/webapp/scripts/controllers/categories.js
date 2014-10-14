cd8a98339e60df7e1be20d3e2bc064810eb7bce3
Leo Orellana <leo.orellana5@gmail.com>
2014-10-14 02:31:11 -0300

    solucionado el mensaje de categoría eliminada

--- desapp-groupc-frontend/src/main/webapp/scripts/controllers/categories.js
+++ desapp-groupc-frontend/src/main/webapp/scripts/controllers/categories.js
@@ -20,10 +20,10 @@ function CategoryControllerNew($scope, $http){
 
 function CategoryControllerDelete($scope, $http, $routeParams) {
 			$http.get("http://localhost:8081/backend/rest/categoryService/delete/"+$routeParams.categoryId)
-			.succes(function(response){
-				$scope.message = 'The category <' + response.name + '> has been deleted';
+			.success(function(response){
+				$scope.message = "The category <" + response.name + "> has been deleted";
 			}).error(function(){
 				console.log("error");
-				$scope.message = 'Error!';
+				$scope.message = "Error!";
 			});
 }
